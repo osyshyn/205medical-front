6501ef1cf451d66a4d0ff424bfae889e8b3bdec9
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-11-11 23:20:53 +0200

    fix

--- src/page-components/dashboard/DashboardMetrics/index.tsx
+++ src/page-components/dashboard/DashboardMetrics/index.tsx
@@ -3,7 +3,7 @@ import { APPROVALS_METRICS, SHIPMENTS_METRICS } from "./constants";
 import { Metrics } from "./Metrics";
 
 export const DashboardMetrics: FC = () => (
-  <section className="flex justify-center gap-6">
+  <section className="flex gap-6">
     <Metrics {...APPROVALS_METRICS} />
     <Metrics {...SHIPMENTS_METRICS} />
   </section>
