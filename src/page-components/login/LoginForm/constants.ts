533627ff0792866c7b1281869b09bd1e962e4efa
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-11-30 00:24:59 +0200

    delete full name

--- src/page-components/login/LoginForm/constants.ts
+++ src/page-components/login/LoginForm/constants.ts
@@ -2,7 +2,6 @@ import * as Yup from "yup";
 import { TextInput } from "src/components/FormField/TextInput";
 import {
   EMAIL_VALIDATION_SCHEMA,
-  FULL_NAME_VALIDATION_SCHEMA,
   PASSWORD_VALIDATION_SCHEMA,
 } from "src/constants/formValidation";
 import { IRenderFormField } from "src/@types/form";
@@ -17,15 +16,6 @@ export const AUTH_INITIAL_VALUES: IFormikValues = {
 };
 
 export const AUTH_FORM_FIELDS: IRenderFormField[] = [
-  {
-    name: "full_name",
-    type: "text",
-    label: "Full Name",
-    component: TextInput,
-    labelClassName: LABEL_CLASSNAME,
-    placeholder: "Enter your name",
-    className: TEXT_INPUT_CLASSNAME,
-  },
   {
     name: "email",
     type: "email",
@@ -47,7 +37,6 @@ export const AUTH_FORM_FIELDS: IRenderFormField[] = [
 ];
 
 export const AUTH_FORM_VALIDATION_SCHEMA = Yup.object().shape({
-  full_name: FULL_NAME_VALIDATION_SCHEMA,
   email: EMAIL_VALIDATION_SCHEMA,
   password: PASSWORD_VALIDATION_SCHEMA,
 });
