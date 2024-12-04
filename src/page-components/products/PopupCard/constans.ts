a2a0748bfade6a280803e4f5ca210ded1c53fa35
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-04 23:13:02 +0200

    add formik to PopupCard

---
+++ src/page-components/products/PopupCard/constans.ts
@@ -0,0 +1,28 @@
+import * as Yup from "yup";
+import { IOptionSelect } from "src/@types/form";
+import { IFormikValues } from "./types";
+
+export const LOCATION_OPTIONS_SELECT: IOptionSelect[] = [
+  { label: "January", value: 1 },
+  { label: "February", value: 2 },
+  { label: "March", value: 3 },
+  { label: "April", value: 4 },
+  { label: "May", value: 5 },
+  { label: "June", value: 6 },
+  { label: "July", value: 7 },
+  { label: "August", value: 8 },
+  { label: "September", value: 9 },
+  { label: "October", value: 10 },
+  { label: "November", value: 11 },
+  { label: "December", value: 12 },
+];
+
+export const PURCHASE_ORDER_INITIAL_VALUES: IFormikValues = {
+  orderLocation: LOCATION_OPTIONS_SELECT[0],
+  poNumber: "",
+};
+
+export const PURCHASE_ORDER_VALIDATION_SCHEMA = Yup.object().shape({
+  orderLocation: Yup.string().required("This field is required."),
+  poNumber: Yup.string().required("This field is required."),
+});
--- src/page-components/products/PopupCard/index.tsx
+++ src/page-components/products/PopupCard/index.tsx
@@ -1,20 +1,69 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { Form, FormikConfig, FormikProvider, useFormik } from "formik";
 import { Button } from "src/components/Button";
-import { Window } from "src/components/Window";
+import { ButtonVariants } from "src/components/Button/types";
+import { TextInput } from "src/components/FormField/TextInput";
+import { CloseButton } from "src/components/ModalWindow/CloseButton";
+import { SortingDropdownList } from "src/components/SortDropdownList";
 import useSidebarStore from "src/stores/siderbar-store";
+import { IOptionSelect } from "src/@types/form";
+import { Sizes } from "src/@types/sizes";
+import {
+  LOCATION_OPTIONS_SELECT,
+  PURCHASE_ORDER_INITIAL_VALUES,
+  PURCHASE_ORDER_VALIDATION_SCHEMA,
+} from "./constans";
+import { IFormikValues } from "./types";
 
 export const PopupCard: FC = () => {
   const isCartOpen = useSidebarStore((state) => state.isCartOpen);
   const closeCart = useSidebarStore((state) => state.closeCart);
 
+  const formikProps: FormikConfig<IFormikValues> = {
+    initialValues: PURCHASE_ORDER_INITIAL_VALUES,
+    validationSchema: PURCHASE_ORDER_VALIDATION_SCHEMA,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  };
+
+  const [location, setLocation] = useState<IOptionSelect>(
+    LOCATION_OPTIONS_SELECT[0]
+  );
+
+  const formik = useFormik(formikProps);
+
   if (!isCartOpen) return null;
 
   return (
-    <Window className="h-screen !bg-black-base">
-      <Button className="text-center text-white-base" onClick={closeCart}>
-        CLOSE
-      </Button>
-      <p className="text-center text-white-base">PopupCard</p>
-    </Window>
+    <div className="h-screen w-full max-w-md rounded-r-3xl border border-gray-soft bg-white-base px-7.5 py-10">
+      <div className="flex items-center justify-between">
+        <h3>Purchase Order</h3>
+        <CloseButton onClose={closeCart} />
+      </div>
+
+      <FormikProvider value={formik}>
+        <Form className="flex flex-col gap-7.5">
+          <div className="mb-7.5 mt-10 flex flex-col gap-3">
+            <SortingDropdownList
+              options={LOCATION_OPTIONS_SELECT}
+              activeOption={location}
+              setOption={setLocation}
+              headLabel="Purchase Order Location"
+            />
+
+            <TextInput
+              className="text-sm"
+              placeholder="Add PO Number"
+              name="poNumber"
+            />
+          </div>
+
+          <Button size={Sizes.S} variant={ButtonVariants.PRIMARY} type="submit">
+            Issue PO
+          </Button>
+        </Form>
+      </FormikProvider>
+    </div>
   );
 };
