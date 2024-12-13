a61931afadb635da09316dcbbbfec1821f95db5b
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-13 19:11:57 +0200

    add SelectDate to ProductsHistory

--- src/pages/Products/ProductsHistory/index.tsx
+++ src/pages/Products/ProductsHistory/index.tsx
@@ -1,11 +1,53 @@
 import React, { FC } from "react";
 import { PageWrapper } from "src/components/Layouts/PageWrapper";
 import { RecentOrders } from "src/components/RecentOrders";
+import { SelectDate } from "src/components/SelectDate";
+import {
+  getCurrentMonthOption,
+  getCurrentYearOption,
+  MONTH_OPTIONS_SELECT,
+  YEARS_OPTIONS_SELECT,
+} from "src/components/SelectDate/constants";
+import { useQueryParams } from "src/hooks/useQueryParams";
+import { QUERY_PARAM_KEYS } from "src/constants/queryParams";
+import { IOptionSelect } from "src/@types/form";
 
-const ProductsHistory: FC = () => (
-  <PageWrapper>
-    <RecentOrders />
-  </PageWrapper>
-);
+const ProductsHistory: FC = () => {
+  const { getQueryParam, setMultipleQueryParams } = useQueryParams();
+
+  const selectMonthOption = MONTH_OPTIONS_SELECT.find(
+    ({ value }) => value === +getQueryParam(QUERY_PARAM_KEYS.MONTH)
+  );
+
+  const selectYearOption = YEARS_OPTIONS_SELECT.find(
+    ({ value }) => value === +getQueryParam(QUERY_PARAM_KEYS.YEAR)
+  );
+
+  const setSelectMonthOption = ({ value }: IOptionSelect) =>
+    setMultipleQueryParams({
+      [QUERY_PARAM_KEYS.MONTH]: value,
+      [QUERY_PARAM_KEYS.PAGE]: "1",
+    });
+
+  const setSelectYearOption = ({ value }: IOptionSelect) => {
+    setMultipleQueryParams({
+      [QUERY_PARAM_KEYS.YEAR]: value,
+      [QUERY_PARAM_KEYS.PAGE]: "1",
+    });
+  };
+
+  return (
+    <PageWrapper mainClassName="flex flex-col gap-10">
+      <SelectDate
+        selectMonth={selectMonthOption || getCurrentMonthOption()}
+        setSelectMonth={setSelectMonthOption}
+        selectYear={selectYearOption || getCurrentYearOption()}
+        setSelectYear={setSelectYearOption}
+      />
+
+      <RecentOrders />
+    </PageWrapper>
+  );
+};
 
 export default ProductsHistory;
