f6191772af43fec3185fe710fcdbb5974b735047
cyber-boxer <kozakvolodymyr21@gmail.com>
2025-01-08 16:25:14 +0200

    add page

--- src/pages/Reporting/PurchaseAnalytics.tsx
+++ src/pages/Reporting/PurchaseAnalytics.tsx
@@ -12,11 +12,21 @@ import {
   PURCHASE_BY_CATEGORY_COLUMNS,
 } from "src/page-components/reporting-analytics/purchasesByProductsList/constants";
 import { PageWrapper } from "src/components/Layouts/PageWrapper";
+import { SelectDate } from "src/components/SelectDate";
+import {
+  getCurrentMonthOption,
+  getCurrentYearOption,
+  MONTH_OPTIONS_SELECT,
+  YEARS_OPTIONS_SELECT,
+} from "src/components/SelectDate/constants";
 import { Table, TableBody, TableHeader } from "src/components/Table";
 import { Title } from "src/components/Title";
 import { Window } from "src/components/Window";
+import { useQueryParams } from "src/hooks/useQueryParams";
 import useCategoryStore from "src/stores/category-store";
 import useProductStore from "src/stores/product-store";
+import { QUERY_PARAM_KEYS } from "src/constants/queryParams";
+import { IOptionSelect } from "src/@types/form";
 import { Row } from "src/@types/table";
 
 export const PurchaseAnalytics: FC = () => {
@@ -38,6 +48,29 @@ export const PurchaseAnalytics: FC = () => {
   const purchasesByProductList = useProductStore(
     (state) => state.purchasesByProductList
   );
+
+  const { getQueryParam, setMultipleQueryParams } = useQueryParams();
+
+  const selectMonthOption = MONTH_OPTIONS_SELECT.find(
+    ({ value }) => value === +getQueryParam(QUERY_PARAM_KEYS.MONTH)
+  );
+
+  const selectYearOption = YEARS_OPTIONS_SELECT.find(
+    ({ value }) => value === +getQueryParam(QUERY_PARAM_KEYS.YEAR)
+  );
+
+  const setSelectMonthOption = ({ value }: IOptionSelect) =>
+    setMultipleQueryParams({
+      [QUERY_PARAM_KEYS.MONTH]: value,
+      [QUERY_PARAM_KEYS.PAGE]: "1",
+    });
+
+  const setSelectYearOption = ({ value }: IOptionSelect) => {
+    setMultipleQueryParams({
+      [QUERY_PARAM_KEYS.YEAR]: value,
+      [QUERY_PARAM_KEYS.PAGE]: "1",
+    });
+  };
 
   useEffect(() => {
     loadPurchaseByCategoryList({ month: 12, year: 2024 });
@@ -62,6 +95,12 @@ export const PurchaseAnalytics: FC = () => {
 
   return (
     <PageWrapper>
+      <SelectDate
+        selectMonth={selectMonthOption || getCurrentMonthOption()}
+        setSelectMonth={setSelectMonthOption}
+        selectYear={selectYearOption || getCurrentYearOption()}
+        setSelectYear={setSelectYearOption}
+      />
       <div className="mt-5 flex gap-6">
         <Window className="max-h-150 w-1/2 overflow-auto">
           <Title title="Purchase by ctegory" subtitle="" />
@@ -81,10 +120,18 @@ export const PurchaseAnalytics: FC = () => {
           </Table>
         </Window>
       </div>
-      <Table>
-        <TableHeader columns={PURCHASE_BY_PRODUCTS_LIST_COLUMNS} />
-        <TableBody items={items} columns={PURCHASE_BY_PRODUCTS_LIST_COLUMNS} />
-      </Table>
+      <div className="mt-10">
+        <Window>
+          <Title title="Products" subtitle="" />
+          <Table>
+            <TableHeader columns={PURCHASE_BY_PRODUCTS_LIST_COLUMNS} />
+            <TableBody
+              items={items}
+              columns={PURCHASE_BY_PRODUCTS_LIST_COLUMNS}
+            />
+          </Table>
+        </Window>
+      </div>
     </PageWrapper>
   );
 };
